9ed714588e99f11e359d6f50563b964060d84078
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2015-02-16 17:06:18 +0000

    removed cached content

---
+++ public/javascripts/application.js
@@ -0,0 +1,12 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+//= require_tree ./dxhmltree
+//= require_tree ./jqgrid
+//= require_tree ./viewxml
+//= require jquery
+//= require jquery_ujs
+//= require jquery_migrate
+//= require twitter/bootstrap
+//= require jquery-fileupload
+//= require_tree .
